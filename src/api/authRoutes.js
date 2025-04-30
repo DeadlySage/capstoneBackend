3adbc9602d1769a2cf93b147f1bd033248f6799a
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-30 13:36:34 -0400

    removed comments

--- src/api/authRoutes.js
+++ src/api/authRoutes.js
@@ -1,23 +1,8 @@
 const router = require("express").Router();
-// const PORT = 3000;
-// app.use(express.json());
+
 const { createUser, userLogIn } = require("./auth");
 
 router.post("/register", createUser);
 router.post("/login", userLogIn);
-// app.post("/api/auth/register", async (req, res, next) => {
-//   try {
-//     await createUser(req, res, next);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-// app.post("/api/auth/login", async (req, res, next) => {
-//   try {
-//     await userLogIn(req, res, next);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 module.exports = router;
