8030063b1c4aa25f48a042cd286d70b8e6c1a34a
DeadlySage <adrian.hartanto159@gmail.com>
2025-05-14 22:05:21 -0700

    added a seed.js file to populate ServiceIntervalRule table

---
+++ src/seed.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+const prisma = new PrismaClient();
+
+// ServiceIntervalRule defaults
+await prisma.serviceIntervalRule.deleteMany();
+
+await prisma.serviceIntervalRule.createMany({
+  data: [
+    { 
+      serviceType: "EngineOilAndFilterChange",
+      description:
+        "Replaces engine oil and filter to lubricate engine components, reduce friction, and remove contaminants. Essential for engine health and performance. Interval can vary significantly with oil type (conventional/synthetic) and vehicle model.",
+      defaultIntervalMiles: 5000,
+    },
+    {
+      serviceType: "TireRotation",
+      description:
+        "Rotating tires promotes even tread wear, extending tire life and maintaining balanced handling.",
+      defaultIntervalMiles: 7500,
+    },
+    {
+      serviceType: "breakPadsChange",
+      description:
+        "worn out break pads can be dangerous, especially in rainy conditions",
+      defaultIntervalMiles: 15000,
+    },
+  ],
+});
